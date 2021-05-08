517b524931575ecd9d35ae6b8d2accd844421650
Sebastian_Warvsten <sebastian.warvsten@iths.se>
2021-05-08 14:44:46 +0200

    Flag showing next to contryname

--- src/views/Contylist.vue
+++ src/views/Contylist.vue
@@ -3,7 +3,9 @@
         <h1>All contries in alphabetical order</h1>
         <input type="textfield" placeholder="Antal länder som visas" v-model="number">
         <div v-if="cities != null">
-            <div v-for="(city, index) in cities.slice(0, number)" :key="city">{{index+1}} {{city.name}} <img alt="country flag" src="city.flag"></div>
+            <div v-for="(city, index) in cities.slice(0, number)" :key="city.alpha3Code">
+                {{index+1}} {{city.name}} <img alt="country flag" :src="city.flag" height="30" width="50">
+            </div>
         </div>
         <div v-else>
             Ange antal länder
@@ -21,7 +23,8 @@ export default {
     data(){
         return{
             cities: null,
-            number: Number(11)
+            number: Number(11),
+            alpha3Code: null
         }
     },
     el: '#app',
@@ -30,6 +33,7 @@ export default {
             axios.get('https://restcountries.eu/rest/v2/')
             .then(response => {
                 this.cities = response.data
+                console.log(this.cities)
             })
         },
     },
