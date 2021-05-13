c32e40120af501183a6dc40d80283f23c409bec8
Sebastian_Warvsten <sebastian.warvsten@iths.se>
2021-05-13 16:46:17 +0200

    Bildhämtning via mutation och bilder borttagna

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -6,15 +6,20 @@
     <br>
     När man klickar på en bild så ska temat ändras, ex klickar man på en The Settlers 3 bild så ska temat bli stone/marble
     <h3>Saker kvar att använda:</h3>
-Minst en komponent tar emot, och använder, minst en prop (via props-nyckeln)<br>
-    <h1>Why I do this</h1>
-    <h2>Criterias:</h2>
-    <div v-for="criteria in criterias" :key="criteria.name">
+    <br>
+    <h1>Byta bilder via mutation</h1>
+    <img :src="picture"><br>
+    Picture #: {{$store.state.index+1}}<br>
+    
+    <button @click="$store.commit('changeIndex')">Byt Bild</button>
+    <h2>Criterias hämtade från lokal json-fil:</h2>
+        <div v-for="criteria in criterias" :key="criteria.name">
       <label>
         {{criteria.name}}
       <input type="checkbox">
-    </label>
+      </label>
     </div>
+    
 </div>
 </template>
 
@@ -22,15 +27,23 @@ Minst en komponent tar emot, och använder, minst en prop (via props-nyckeln)<br
 import criteria from '../assets/criteria.json'
 
 export default {
+  computed:{
+    picture(){
+      return this.$store.state.pictures[this.$store.state.index]
+    }
+  },
   data(){
     return{
       criterias: criteria
     }
   },
+  methods:{
+
+  },
   name: 'Homepage',
   props: {
     msg: String,
-  }
+  },
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    index: 0,
+    pictures: [
+      'https://picsum.photos/1024/480/?image=13',
+      'https://picsum.photos/1024/480/?image=14',
+      'https://picsum.photos/1024/480/?image=15',
+      'https://picsum.photos/1024/480/?image=16',
+      'https://picsum.photos/1024/480/?image=17',
+      'https://picsum.photos/1024/480/?image=18',
+      'https://picsum.photos/1024/480/?image=19',
+      
+    ]
   },
   mutations: {
+    changeIndex(state){
+      if(this.state.index < this.state.pictures.length-1){
+        state.index++
+      }else{
+        state.index = 0
+      }
+    }
   },
   actions: {
   },
